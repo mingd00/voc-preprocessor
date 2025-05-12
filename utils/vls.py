39a89bc5ce98c7a6d7535b6ca56d4d87c71659d4
KMJ <rkdalswl403@naver.com>
2025-05-12 14:38:59 +0900

    Bounding Box 시각화

---
+++ utils/vls.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+def draw_bbox(ax, img, annot):
+    ax.imshow(img)
+    ax.axis('off')
+
+    bboxes = annot['boxes']
+    for bbox in bboxes:
+        xmin, ymin, xmax, ymax = bbox
+        rect = Rectangle((xmin, ymin), width=xmax-xmin, height=ymax-ymin, facecolor='none', edgecolor='r')
+        ax.add_patch(rect)
